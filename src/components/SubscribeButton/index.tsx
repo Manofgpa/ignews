1f21710a3940fa9f816fee42c2473d40c067a88d
Felipe Mano <manofgpa@gmail.com>
2021-10-01 14:08:01 -0300

    Add Stripe duplicated customers verification

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,42 @@
 import styles from './styles.module.scss'
+import { useSession, signIn } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripe } from '../../services/stripe-js'
 
 interface SubscribeButtonProps {
   priceId: string
 }
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const [session] = useSession()
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripe()
+
+      await stripe.redirectToCheckout({
+        sessionId,
+      })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
-    <button type='button' className={styles.subscribeButton}>
+    <button
+      type='button'
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   )
